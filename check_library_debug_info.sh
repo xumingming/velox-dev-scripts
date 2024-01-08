3350f10ae8754d3e8f1bf2dc82995ba9dbdc65df
James Xu <xumingmingv@gmail.com>
2024-01-08 11:15:53 +0800

    Update presto-cli.sh

---
+++ check_library_debug_info.sh
@@ -0,0 +1,4 @@
+ 
+ # https://stackoverflow.com/questions/1999654/how-can-i-tell-if-a-library-was-compiled-with-g
+ # Mac.
+ dwarfdump --debug-info library_name.dylib
--- presto-cli.sh
+++ presto-cli.sh
@@ -1,1 +1,1 @@
-java -jar $PRESTO_SRC_HOME/presto-cli/target/presto-cli-*-executable.jar --catalog tpchstandard
+java -jar $PRESTO_SRC_HOME/presto-cli/target/presto-cli-*-executable.jar --catalog hive
